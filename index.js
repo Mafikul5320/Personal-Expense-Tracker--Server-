e941e09d21eaf87b2a07a7ae9f252d521336ad88
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-08-15 19:07:54 +0600

    Server setup done

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(cors());
+app.use(express.json())
+const port = process.env.PORT || 3000
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const database = client.db("Trackify")
+        const expenseCollection = database.collection("Expense")
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Trackify is C o o k i n g')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
